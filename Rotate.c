ab1000141c9b32ba70a68594c6e561e2aeb55642
Vinay Kumar <24tec2aids079@vgu.ac.in>
2026-03-17 14:05:17 +0530

    Rotate method in array
    Using Extra Array

---
+++ Rotate.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void rotateArray(int arr[], int n, int k) {
+    int temp[n];
+    
+    for(int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = 7;
+    int k = 3;
+
+    rotateArray(arr, n, k);
+
+    printf("Rotated Array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
